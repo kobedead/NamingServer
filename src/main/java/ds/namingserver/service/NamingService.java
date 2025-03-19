5b39eced0677d03321ac79a77d4f20bd04b607c5
s0193227 <eli.wauters@student.uantwerpen.be>
2025-03-19 19:14:40 +0100

    initial

---
+++ src/main/java/ds/namingserver/service/NamingService.java
@@ -0,0 +1,13 @@
+package ds.namingserver.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NamingService {
+
+    /// Hashing function to hash incoming names (based on given hashing algorithm)
+
+    /// Function to add new node (validate if node already exists)
+
+
+}
