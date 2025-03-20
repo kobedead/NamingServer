48cffea24196ff9223403d26382ccf858cac6569
s0193227 <eli.wauters@student.uantwerpen.be>
2025-03-20 10:14:02 +0100

    added some basic hashing functionality

---
+++ src/test/java/ds/namingserver/service/NamingServiceTest.java
@@ -0,0 +1,19 @@
+package ds.namingserver.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class NamingServiceTest {
+    @Autowired
+    private NamingService namingService;
+
+    @Test
+    public void testNamingService() {
+        namingService.addNode("testNode1", "192.168.1.1");
+    }
+
+}
