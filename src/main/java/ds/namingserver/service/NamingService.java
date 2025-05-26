0994db7bf0f55bb401c40c28e2bff09a63325d49
s0193227 <eli.wauters@student.uantwerpen.be>
2025-05-27 00:46:01 +0200

    Revert "FailureFix"
    This reverts commit 109d1abbc1e1001413ddcf468fb43d9464662ad7.

--- src/main/java/ds/namingserver/service/NamingService.java
+++ src/main/java/ds/namingserver/service/NamingService.java
@@ -3,8 +3,6 @@ package ds.namingserver.service;
 import ds.namingserver.Config.NSConf;
 import ds.namingserver.CustomMap.LocalJsonMap;
 import ds.namingserver.Multicast.MulticastListener;
-import ds.namingserver.Utilities.NextAndPreviousNodeDTO;
-import ds.namingserver.Utilities.NodeDTO;
 import ds.namingserver.Utilities.Utilities;
 import jakarta.annotation.PostConstruct;
 import org.springframework.core.io.Resource;
@@ -313,18 +311,28 @@ public class NamingService {
      * @param id id of node to look for neighbors
      * @return next and previous node in map.
      */
-    public NextAndPreviousNodeDTO getNextAndPreviousDTO(Integer id) {
+    public Map<Integer, String> getNextAndPrevious(Integer id) {
+
+        Map<Integer, String> nextAndPrevMap = new HashMap<>();
+
+
         int nextKey = map.getNextKeyWithWrap(id);
         int previousKey = map.getPreviousKeyWithWrap(id);
 
-        NodeDTO nextNode = new NodeDTO(map.getNextKeyWithWrap(id) , map.get(map.getNextKeyWithWrap(id)));
-        NodeDTO previousNode = new NodeDTO(map.getPreviousKeyWithWrap(id) , map.get(map.getPreviousKeyWithWrap(id)));
+        if (nextKey == previousKey){
+            nextAndPrevMap.put(nextKey,map.get(nextKey));
+        }
+        else {
+            nextAndPrevMap.put(nextKey, map.get(nextKey));
+            nextAndPrevMap.put(previousKey, map.get(previousKey));
+        }
 
-        NextAndPreviousNodeDTO dto = new NextAndPreviousNodeDTO(nextNode, previousNode);
+        System.out.println("Next and previous : " + nextAndPrevMap +  "   , asked of node : " + id);
+        System.out.println("Full map is : " + map.toString());
 
-        System.out.println("Next and previous (DTO): Next=" + nextKey + ", Previous=" + previousKey + " , asked of node : " + id);
 
-        return dto;
+        return nextAndPrevMap;
+
     }
 
     public int getNumberOfNodes() {
